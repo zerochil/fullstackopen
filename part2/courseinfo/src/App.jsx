7215730de605407f8e84cb34da3a8444f8c22263
reda <redorangin@gmail.com>
2023-09-06 07:16:49 +0100

    2.5: separate module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,5 @@
+import Course from './components/Course.jsx'
+
 const App = () => {
   const courses = [
     {
@@ -49,42 +51,6 @@ const App = () => {
       <h1>Web development curriculum</h1>
       {courses.map( course => <Course key={course.id} course={course} /> )}
     </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <h2>{props.course}</h2>
-  );
-};
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      { parts.map (part => <Part key={part.id} part={part} />) }
-    </div>
-  );
-};
-
-const Part = ({part}) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Total = ({parts}) => {
-  return (
-    <p><b>total of {parts.map(part=>part.exercises).reduce( (total, exercises) => exercises+total )} exercises</b></p>
   )
 }
 
