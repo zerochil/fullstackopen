5bc1796b4bc2b7c544df050bd3312ad9b25d39eb
reda <redorangin@gmail.com>
2023-09-07 12:12:52 +0100

    3.7: Phonebook backend step7

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   { 
@@ -70,6 +73,12 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 }) 
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 app.listen(3000, () => {
   console.log('app listening on port 3000')
