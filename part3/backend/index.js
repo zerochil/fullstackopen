227d0a45b12892ff6e9102a9071ba3cb5d696eb8
reda <redorangin@gmail.com>
2023-09-07 12:22:51 +0100

    3.8*: Phonebook backend step8

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('req-body', (req) => JSON.stringify(req.body));
+
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :req-body')
+);
 
 let persons = [
   { 
