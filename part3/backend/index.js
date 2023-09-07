0c7db1e28b81cf998c14d9c9fde25e53ee154a95
reda <redorangin@gmail.com>
2023-09-07 10:47:59 +0100

    3.4: Phonebook backend step4

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -47,7 +47,6 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
   persons = persons.filter(person => person.id != id )
-
   response.status(204).end()
 })
 
