d0fc0cebf6eba7ffc3a73f589a09f0f060cea398
reda <redorangin@gmail.com>
2023-09-07 12:29:14 +0100

    change of style

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
 
 morgan.token('req-body', (req) => JSON.stringify(req.body));
 
 
-app.use(express.json())
+app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :req-body')
 );
@@ -31,60 +31,60 @@ let persons = [
     "name": "Mary Poppendieck", 
     "number": "39-23-6423122"
   }
-]
+];
 
 app.get('/info/', (request, response) => {
-  const d = new Date()
-  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${d.toString()}</p>`)
-})
+  const d = new Date();
+  response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${d.toString()}</p>`);
+});
 
 app.get('/api/persons/', (request, response) => {
-  response.json(persons)
-})
+  response.json(persons);
+});
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id == id) 
+  const id = request.params.id;
+  const person = persons.find(person => person.id == id) ;
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
-      response.status(404).end()
+      response.status(404).end();
   }
-})
+});
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id != id )
-  response.status(204).end()
-})
+  const id = request.params.id;
+  persons = persons.filter(person => person.id != id );
+  response.status(204).end();
+});
 
 app.post('/api/persons', (request, response) => {
-  const person = request.body
+  const person = request.body;
 
   if (!person.name || !person.number) {
     return response.status(400).json({ 
       error: 'missing person\'s details.' 
-    })
-  }
+    });
+  };
 
   if (persons.some(p => p.name == person.name)) {
     return response.status(400).json({ 
       error: 'This name exists already.' 
-    })
-  }
+    });
+  };
 
-  person.id = Math.floor(Math.random()*10000000)
-  persons = persons.concat(person)
+  person.id = Math.floor(Math.random()*10000000);
+  persons = persons.concat(person);
 
-  response.json(person)
-}) 
+  response.json(person);
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
 
 app.listen(3000, () => {
-  console.log('app listening on port 3000')
-})
+  console.log('app listening on port 3000');
+});
