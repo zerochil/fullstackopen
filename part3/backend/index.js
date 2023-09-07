03e1a4457260eb47be91709e083cb9d43cf01a11
reda <redorangin@gmail.com>
2023-09-07 12:55:28 +0100

    fixed CORS

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 morgan.token('req-body', (req) => JSON.stringify(req.body));
 
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :req-body')
